20117700c7242910d3ef616950baff576d31fd65
Zac Crumpton <z.crumpton@gmail.com>
2020-02-28 23:14:10 -0600

    added bearForm, folders, printToDom function, and bearFormData

---
+++ javascripts/helpers/utils.js
@@ -0,0 +1,8 @@
+//printToDom function
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+};
+
+export default { printToDom };
